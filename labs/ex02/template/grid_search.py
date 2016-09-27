533e45cbab4fc7f262fb5ccddbbca1f85c4b6dc4
Martin Jaggi <m.jaggi@gmail.com>
2016-09-27 11:19:04 +0200

    missing helper file for code template of lab 2

---
+++ labs/ex02/template/grid_search.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Exercise 2.
+
+Grid Search
+"""
+
+import numpy as np
+import costs
+
+
+def generate_w(num_intervals):
+    """Generate a grid of values for w0 and w1."""
+    w0 = np.linspace(-100, 200, num_intervals)
+    w1 = np.linspace(-150, 150, num_intervals)
+    return w0, w1
+
+
+def get_best_parameters(w0, w1, losses):
+    """Get the best w from the result of grid search."""
+    min_row, min_col = np.unravel_index(np.argmin(losses), losses.shape)
+    return losses[min_row, min_col], w0[min_row], w1[min_col]
+
+
+# ***************************************************
+# INSERT YOUR CODE HERE
+# TODO: Paste your implementation of grid_search 
+#       here when it is done.
+# ***************************************************
