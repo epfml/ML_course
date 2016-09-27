1acbd2c705387cf7a79a96780e62efcc738d61dd
Frederik Kunstner <frederik.kunstner@gmail.com>
2016-09-27 18:30:04 +0200

    Add missing template files

---
+++ labs/ex02/template/costs.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""a function used to compute the cost."""
+
+import numpy as np
+
+def compute_cost(y, tx, w):
+    """Calculate the cost.
+
+    You can calculate the cost using mse or mae.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE / MAE
+    # ***************************************************
+    raise NotImplementedError
