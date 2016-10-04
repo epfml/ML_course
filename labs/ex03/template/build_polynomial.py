ea25510f8aefa0bf0323643bd5a31f30f556cb1e
Tao Lin <lintaog3@gmail.com>
2016-10-04 18:05:32 +0200

    add missing file for ex03

---
+++ labs/ex03/template/build_polynomial.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+
+def build_poly(x, degree):
+    """polynomial basis function."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # polynomial basis function: TODO
+    # this function should return the matrix formed
+    # by applying the polynomial basis to the input data
+    # ***************************************************
+    raise NotImplementedError
