f1108a410cda88e4754140d01bc45855345fe1a6
Martin Jaggi <m.jaggi@gmail.com>
2016-10-05 18:07:35 +0200

    lab03: slight clarification of the polynomial regression question

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def build_poly(x, degree):
-    """polynomial basis function."""
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
     # ***************************************************
     # INSERT YOUR CODE HERE
     # polynomial basis function: TODO
