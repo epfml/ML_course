fbcae3ffb4da232eb7dc98a43d1a0d877f384a00
yaoyuan <yaoyuan0608@gmail.com>
2017-10-06 22:31:58 +0200

    update up to exercise3

--- labs/ex03/template/costs.py
+++ labs/ex03/template/costs.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """Function used to compute the loss."""
 
-
+import numpy as np
     
-def compute_loss(y, tx, w):
+def compute_mse(y, tx, w):
     """Calculate the loss.
 
     You can calculate the loss using mse or mae.
@@ -14,5 +14,6 @@ def compute_loss(y, tx, w):
     # ***************************************************
     e = y - np.dot(tx, w)
     N = len(e)
-    MSE_loss = 1/N * sum(e**2)
+    MSE_loss = 0.5/N * sum(e**2)
+    
     return MSE_loss
