805808827c40d29661eecd8eba27ff6df8f861f9
Martin Jaggi <m.jaggi@gmail.com>
2019-09-24 14:51:18 +0200

    lab 2 and solutions lab 1

---
+++ labs/ex02/template/stochastic_gradient_descent.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Stochastic Gradient Descent"""
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: implement stochastic gradient computation.It's same as the gradient descent.
+    # ***************************************************
+    raise NotImplementedError
+
+
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent algorithm."""
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: implement stochastic gradient descent.
+    # ***************************************************
+    raise NotImplementedError
+    return losses, ws
