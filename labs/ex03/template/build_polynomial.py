6ca5d9a6c4b0b1d356a904d5e5f719d2c9018b11
Alix <alix.jeannerot@epfl.ch>
2019-10-03 17:31:00 +0200

    beginning of the poly

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -6,10 +6,16 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    # *    """polynomial basis functions for input data x, for j=0 up to j=degree."""
     # ***************************************************
     # INSERT YOUR CODE HERE
     # polynomial basis function: TODO
     # this function should return the matrix formed
     # by applying the polynomial basis to the input data
     # ***************************************************
-    raise NotImplementedError
+    #rx=np.repeat(x,degree, axis=1)
+    rx=[x for _ in range(degree)]
+    print(rx)
+    for i in range(degree):
+        rx[i]=np.power(x,i)
+    return rx
