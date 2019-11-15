24d20d942e90c3b1a9f25e79d92aeae2ffb3deba
Thijs Vogels <thijs.vogels@epfl.ch>
2019-11-15 16:44:57 +0100

    Project 2 - road segmentation - update to Python 3

--- projects/project2/project_road_segmentation/Datasets/mask_to_submission.py
+++ projects/project2/project_road_segmentation/Datasets/mask_to_submission.py
@@ -5,7 +5,9 @@ import numpy as np
 import matplotlib.image as mpimg
 import re
 
-foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
+foreground_threshold = (
+    0.25
+)  # percentage of pixels > 1 required to assign a foreground label to a patch
 
 # assign a label to a patch
 def patch_to_label(patch):
@@ -23,24 +25,24 @@ def mask_to_submission_strings(image_filename):
     patch_size = 16
     for j in range(0, im.shape[1], patch_size):
         for i in range(0, im.shape[0], patch_size):
-            patch = im[i:i + patch_size, j:j + patch_size]
+            patch = im[i : i + patch_size, j : j + patch_size]
             label = patch_to_label(patch)
-            yield("{:03d}_{}_{},{}".format(img_number, j, i, label))
+            yield ("{:03d}_{}_{},{}".format(img_number, j, i, label))
 
 
 def masks_to_submission(submission_filename, *image_filenames):
     """Converts images into a submission file"""
-    with open(submission_filename, 'w') as f:
-        f.write('id,prediction\n')
+    with open(submission_filename, "w") as f:
+        f.write("id,prediction\n")
         for fn in image_filenames[0:]:
-            f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn))
+            f.writelines("{}\n".format(s) for s in mask_to_submission_strings(fn))
 
 
-if __name__ == '__main__':
-    submission_filename = 'dummy_submission.csv'
+if __name__ == "__main__":
+    submission_filename = "dummy_submission.csv"
     image_filenames = []
     for i in range(1, 51):
-        image_filename = 'training/groundtruth/satImage_' + '%.3d' % i + '.png'
-        print image_filename
+        image_filename = "training/groundtruth/satImage_" + "%.3d" % i + ".png"
+        print(image_filename)
         image_filenames.append(image_filename)
     masks_to_submission(submission_filename, *image_filenames)
--- projects/project2/project_road_segmentation/Datasets/submission_to_mask.py
+++ projects/project2/project_road_segmentation/Datasets/submission_to_mask.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 import os
 import sys
-import Image
+from PIL import Image
 import math
 import matplotlib.image as mpimg
 import numpy as np
 
-label_file = 'dummy_submission.csv'
+label_file = "sample_submission.csv"
 
 h = 16
 w = h
-imgwidth = int(math.ceil((600.0/w))*w)
-imgheight = int(math.ceil((600.0/h))*h)
+imgwidth = int(math.ceil((600.0 / w)) * w)
+imgheight = int(math.ceil((600.0 / h)) * h)
 nc = 3
 
 # Convert an array of binary labels to a uint8
@@ -19,36 +19,37 @@ def binary_to_uint8(img):
     rimg = (img * 255).round().astype(np.uint8)
     return rimg
 
+
 def reconstruct_from_labels(image_id):
     im = np.zeros((imgwidth, imgheight), dtype=np.uint8)
     f = open(label_file)
     lines = f.readlines()
-    image_id_str = '%.3d_' % image_id
+    image_id_str = "%.3d_" % image_id
     for i in range(1, len(lines)):
         line = lines[i]
         if not image_id_str in line:
             continue
 
-        tokens = line.split(',')
+        tokens = line.split(",")
         id = tokens[0]
         prediction = int(tokens[1])
-        tokens = id.split('_')
+        tokens = id.split("_")
         i = int(tokens[1])
         j = int(tokens[2])
 
-        je = min(j+w, imgwidth)
-        ie = min(i+h, imgheight)
+        je = min(j + w, imgwidth)
+        ie = min(i + h, imgheight)
         if prediction == 0:
-            adata = np.zeros((w,h))
+            adata = np.zeros((w, h))
         else:
-            adata = np.ones((w,h))
+            adata = np.ones((w, h))
 
         im[j:je, i:ie] = binary_to_uint8(adata)
 
-    Image.fromarray(im).save('prediction_' + '%.3d' % image_id + '.png')
+    Image.fromarray(im).save("prediction_" + "%.3d" % image_id + ".png")
 
     return im
 
+
 for i in range(1, 5):
     reconstruct_from_labels(i)
-   
