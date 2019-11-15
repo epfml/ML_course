10e2593a6676acb33332d508643283c2b708d0fd
Wanhao Zhou <wanhao.zhou@gmail.com>
2019-11-15 15:20:44 +0100

    update imread and plot functions

--- labs/ex08/solution/plots.py
+++ labs/ex08/solution/plots.py
@@ -8,25 +8,24 @@ from helper import build_distance_matrix
 
 
 def plot_cluster(data, mu, colors, ax):
-    """plot the cluster.
+    """
+    plot the cluster.
 
     Note that the dimension of the column vector `colors`
     should be the same as the number of clusters.
     """
-    # check if the dimension matches.
-    assert(len(colors) >= mu.shape[0])
     # build distance matrix.
     distance_matrix = build_distance_matrix(data, mu)
     # get the assignments for each point.
     assignments = np.argmin(distance_matrix, axis=1)
-    #
     for k_th in range(mu.shape[0]):
         rows, cols = np.where(assignments == k_th)
         data_of_kth_cluster = data[rows, :]
         ax.scatter(
             data_of_kth_cluster[:, 0],
             data_of_kth_cluster[:, 1],
-            s=40, c=colors[k_th])
+            # works for clusters more than 3
+            s=40, c=colors[k_th % len(colors)])
     ax.grid()
     ax.set_xlabel("x")
     ax.set_ylabel("y")
@@ -59,13 +58,12 @@ def plot_image_compression(original_image, image, assignments, mu, k):
     """plot histgram."""
     # init the plot
     fig = plt.figure()
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax1.imshow(original_image, cmap='Greys_r')
 
     # visualization
-    image_reconstruct = mu[assignments]
-    # image_reconstruct = np.squeeze(image_reconstruct, axis=1)
-    image_reconstruct = image_reconstruct.astype('uint8').reshape(original_image.shape)
+    image_reconstruct = mu[assignments].reshape(original_image.shape)
+    
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax1.imshow(original_image, cmap='Greys_r')
     ax2 = fig.add_subplot(1, 2, 2)
     ax2.imshow(image_reconstruct, cmap='Greys_r')
     plt.draw()
