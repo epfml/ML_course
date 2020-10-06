e224ebc60e34ebe25b2ae49b641c2b82d270c6b5
nourGhribi <nour.ghribi@epfl.ch>
2020-10-06 18:12:02 +0200

    finished w3 Q&A

--- labs/ex03/template/gradient_descent.py
+++ labs/ex03/template/gradient_descent.py
@@ -22,7 +22,4 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
         # store w and loss
         ws.append(w)
         losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-
     return ws, losses
