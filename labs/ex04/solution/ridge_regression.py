30b7a2009e36df15ad68c8d6ef3017e8684405e5
Nicolas Flammarion <25341034+nflammar@users.noreply.github.com>
2020-10-27 08:24:37 +0100

    new lectures, exercices and solutions

---
+++ labs/ex04/solution/ridge_regression.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Exercise 3.
+
+Ridge Regression
+"""
+
+import numpy as np
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression."""
+    aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
