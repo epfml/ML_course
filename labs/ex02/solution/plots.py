6a480628abf0e0a5e19fa3ee0c580ce304df0261
Martin Jaggi <m.jaggi@gmail.com>
2021-11-02 22:05:36 +0100

    publish solutions labs 2-6

---
+++ labs/ex02/solution/plots.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""function for plot."""
+import matplotlib.pyplot as plt
+import numpy as np
+from grid_search import get_best_parameters
+
+
+def prediction(w0, w1, mean_x, std_x):
+    """Get the regression line from the model."""
+    x = np.arange(1.2, 2, 0.01)
+    x_normalized = (x - mean_x) / std_x
+    return x, w0 + w1 * x_normalized
+
+
+def base_visualization(grid_losses, w0_list, w1_list,
+                       mean_x, std_x, height, weight):
+    """Base Visualization for both models."""
+    w0, w1 = np.meshgrid(w0_list, w1_list)
+
+    fig = plt.figure()
+
+    # plot contourf
+    ax1 = fig.add_subplot(1, 2, 1)
+    cp = ax1.contourf(w0, w1, grid_losses.T, cmap=plt.cm.jet)
+    fig.colorbar(cp, ax=ax1)
+    ax1.set_xlabel(r'$w_0$')
+    ax1.set_ylabel(r'$w_1$')
+    # put a marker at the minimum
+    loss_star, w0_star, w1_star = get_best_parameters(
+        w0_list, w1_list, grid_losses)
+    ax1.plot(w0_star, w1_star, marker='*', color='r', markersize=20)
+
+    # plot f(x)
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.scatter(height, weight, marker=".", color='b', s=5)
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+    ax2.grid()
+
+    return fig
+
+
+def grid_visualization(grid_losses, w0_list, w1_list,
+                       mean_x, std_x, height, weight):
+    """Visualize how the trained model looks like under the grid search."""
+    fig = base_visualization(
+        grid_losses, w0_list, w1_list, mean_x, std_x, height, weight)
+
+    loss_star, w0_star, w1_star = get_best_parameters(
+        w0_list, w1_list, grid_losses)
+    # plot prediciton
+    x, f = prediction(w0_star, w1_star, mean_x, std_x)
+    ax2 = fig.get_axes()[2]
+    ax2.plot(x, f, 'r')
+
+    return fig
+
+
+def gradient_descent_visualization(
+        gradient_losses, gradient_ws,
+        grid_losses, grid_w0, grid_w1,
+        mean_x, std_x, height, weight, n_iter=None):
+    """Visualize how the loss value changes until n_iter."""
+    fig = base_visualization(
+        grid_losses, grid_w0, grid_w1, mean_x, std_x, height, weight)
+
+    ws_to_be_plotted = np.stack(gradient_ws)
+    if n_iter is not None:
+        ws_to_be_plotted = ws_to_be_plotted[:n_iter]
+
+    ax1, ax2 = fig.get_axes()[0], fig.get_axes()[2]
+    ax1.plot(
+        ws_to_be_plotted[:, 0], ws_to_be_plotted[:, 1],
+        marker='o', color='w', markersize=10)
+    pred_x, pred_y = prediction(
+        ws_to_be_plotted[-1, 0], ws_to_be_plotted[-1, 1],
+        mean_x, std_x)
+    ax2.plot(pred_x, pred_y, 'r')
+
+    return fig
---
+++ labs/ex04/solution/split_data.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Exercise 3.
+
+Split the dataset based on the given ratio.
+"""
+
+
+import numpy as np
+
+
+def split_data(x, y, ratio, seed=1):
+    """
+    split the dataset based on the split ratio. If ratio is 0.8 
+    you will have 80% of your data set dedicated to training 
+    and the rest dedicated to testing. If ratio times the number of samples is not round
+    you can use np.floor. Also check the documentation for np.random.permutation,
+    it could be useful.
+    
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        y: numpy array of shape (N,).
+        ratio: scalar in [0,1]
+        seed: integer.
+        
+    Returns:
+        x_tr: numpy array containing the train data.
+        x_te: numpy array containing the test data.
+        y_tr: numpy array containing the train labels.
+        y_te: numpy array containing the test labels.
+        
+    >>> split_data(np.arange(13), np.arange(13), 0.8, 1)
+    (array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]), array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]))
+    """
+    # set seed
+    np.random.seed(seed)
+    ### SOLUTION
+    # generate random indices
+    num_row = len(y)
+    indices = np.random.permutation(num_row)
+    index_split = int(np.floor(ratio * num_row))
+    index_tr = indices[: index_split]
+    index_te = indices[index_split:]
+    # create split
+    x_tr = x[index_tr]
+    x_te = x[index_te]
+    y_tr = y[index_tr]
+    y_te = y[index_te]
+    return x_tr, x_te, y_tr, y_te
+    ### TEMPLATE
+    # # ***************************************************
+    # # COPY YOUR CODE FROM EX03 HERE
+    # # split the data based on the given ratio: TODO
+    # # ***************************************************
+    # raise NotImplementedError
+    ### END SOLUTION
