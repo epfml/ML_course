9b6cf69fdf43a05ca178419496924358857c9d65
Martin Jaggi <m.jaggi@gmail.com>
2022-09-16 23:34:46 +0200

    remove old materials, start 2022

--- labs/ex02/solution/subgradient_mae.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def compute_subgradient_mae(y, tx, w):
-    """Compute the gradient."""
-    err = y - tx.dot(w)
-    grad = -tx.T.dot(np.sign(err)) / len(err)
-    return grad, err
