849b1ff4035d358977fe265e4eb52553f89e23fb
osflo <48621374+osflo@users.noreply.github.com>
2022-10-10 16:49:27 +0200

    ex 3

--- labs/ex02/template/gradient_descent.py
+++ labs/ex02/template/gradient_descent.py
@@ -60,7 +60,5 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
         # store w and loss
         ws.append(w)
         losses.append(loss)
-        print("GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
     return losses, ws
