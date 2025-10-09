c7db8c22d6c250aa2a23aeed0d96b42d76f7bef1
Oğuz Kaan Yüksel <okyksl@gmail.com>
2025-10-09 14:50:11 +0200

    fix the visualization issue with matplotlib 3.10

--- labs/ex05/template/plots.py
+++ labs/ex05/template/plots.py
@@ -5,6 +5,18 @@ import matplotlib.pyplot as plt
 
 from helpers import de_standardize, standardize
 
+def get_facecolors(cs):
+    """gets facecolors from the QuadContourSet object"""
+
+    if hasattr(cs, 'get_facecolor'):
+        return cs.get_facecolor()
+    elif hasattr(cs, 'collections'):
+        return [pc.get_facecolor()[0] for pc in cs.collections]
+    else:
+        raise AttributeError(
+            "The provided contour set object does not have a "
+            "'.get_facecolor()' or '.collections' attribute. "
+        )
 
 def visualization(y, x, mean_x, std_x, w, save_name, is_LR=False):
     """visualize the raw data as well as the classification result."""
@@ -51,7 +63,7 @@ def visualization(y, x, mean_x, std_x, w, save_name, is_LR=False):
     prediction = prediction.reshape((weight.shape[0], height.shape[0]))
     cs = ax2.contourf(hx, hy, prediction, 1)
     proxy = [
-        plt.Rectangle((0, 0), 1, 1, fc=pc.get_facecolor()[0]) for pc in cs.collections
+        plt.Rectangle((0, 0), 1, 1, fc=fc) for fc in get_facecolors(cs)
     ]
     ax2.legend(proxy, ["prediction male", "prediction female"])
 
